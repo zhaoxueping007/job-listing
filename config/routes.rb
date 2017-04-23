47ff02bbeac9e2791b1d5b3a97f41954eb7b6507
zhaoxueping007 <zoeyzhao007@gmail.com>
2017-04-23 14:48:22 +0800

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
         post :hide
       end
 
-      resources :resumes 
+      resources :resumes
     end
   end
 
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
     resources :resumes
   end
 
-  root 'jobs#index'
+  root 'welcome#index'
 end
